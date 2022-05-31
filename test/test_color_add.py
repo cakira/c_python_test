0b2e530718327ad94842c32506b5d834e3a17d10
Akira - Cleber Akira Nakandakare <cakira@cpqd.com.br>
2022-05-30 22:26:07 -0300

    Failing: add test assert(black + red = red)

--- test/test_color_add.py
+++ test/test_color_add.py
@@ -12,5 +12,10 @@ class TestColorAdd(unittest.TestCase):
         environment.set_colors('black', 'black')
         color_result = color_add.get_result()
         color_name_result = webcolors.hex_to_name(color_result)
-
         self.assertEqual('black', color_name_result)
+
+    def test_add_red_and_black(self):
+        environment.set_colors('red', 'black')
+        color_result = color_add.get_result()
+        color_name_result = webcolors.hex_to_name(color_result)
+        self.assertEqual('red', color_name_result)
