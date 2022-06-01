da0c39c057d0ec76299e01b908c9f7973da903d3
Akira - Cleber Akira Nakandakare <cakira@cpqd.com.br>
2022-06-01 01:51:16 -0300

    Complete implementation of color_add in C

--- script/generate_color_add_wrapper.py
+++ script/generate_color_add_wrapper.py
@@ -16,6 +16,29 @@ ffibuilder.set_source(
     sources=['color_add.c'],  # includes pi.c as additional sources
     libraries=[])  # on Unix, link with the math library
 
+ffibuilder.embedding_api('''
+    void get_color_a(char *);
+    void get_color_b(char *);
+''')
+
+ffibuilder.embedding_init_code('''
+    from color_add_wrapper import ffi
+    from environment import environment
+
+    @ffi.def_extern()
+    def get_color_a(result):
+        color = environment.get_color_a()
+        for i in range(len(color)):
+            result[i] = color[i].encode()
+        result[len(color)] = b'\\x00'
+
+    @ffi.def_extern()
+    def get_color_b(result):
+        color = environment.get_color_b()
+        for i in range(len(color)):
+            result[i] = color[i].encode()
+        result[len(color)] = b'\\x00'
+''')
 
 if __name__ == '__main__':
     ffibuilder.compile(tmpdir='ccolor_add')
